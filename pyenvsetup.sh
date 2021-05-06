c990c45e6e8768a49a49d5e97275900aef5fe6bd
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2021-05-06 04:51:27 +0000

    Add files via upload

--- pyenvsetup.sh
+++ pyenvsetup.sh
@@ -1,5 +1,7 @@
+sudo apt-get update
+sudo apt-get ugrade
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev 
-xz-utils libffi-dev liblzma-dev git
+xz-utils libffi-dev liblzma-dev git tk8.6-dev
 curl https://pyenv.run | bash
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
@@ -10,3 +12,7 @@ pyenv global 3.7.0
 python -m pip install wheel
 python -m pip install chatterbot chatterbot_corpus tk
 python -m spacy install en
+curl https://raw.githubusercontent.com/ReCore-sys/chatbotsetup/main/chatbot.py -O
+curl https://raw.githubusercontent.com/ReCore-sys/chatbotsetup/main/bot.png -O
+curl https://raw.githubusercontent.com/ReCore-sys/chatbotsetup/main/db.sqlite3 -O
+
